214b84d49f969deec3e8a174ecbf1a5e7ee901e5
Alex Withington-Smith <alex@awithsmith.com>
2024-12-01 09:27:04 +0000

    test for day01

--- lib/day01.rb
+++ lib/day01.rb
@@ -3,4 +3,34 @@
 require_relative 'base_solver'
 
 class Day01 < BaseSolver
+  def solve_part_1
+    list_differences.sum
+  end
+
+  private
+
+  def lists
+    @lists ||= begin
+      list1 = []
+      list2 = []
+
+      each_line do |line|
+        item1, item2 = parse_line(line)
+        list1 << item1
+        list2 << item2
+      end
+
+      [list1.sort, list2.sort]
+    end
+  end
+
+  def parse_line(line)
+    line.split('  ').map(&:to_i)
+  end
+
+  def list_differences
+    list1, list2 = lists
+
+    list1.zip(list2).map { |a, b| (a - b).abs }
+  end
 end
--- test/test_day01.rb
+++ test/test_day01.rb
@@ -3,15 +3,46 @@
 require_relative 'test_helper'
 
 class TestDay01 < Minitest::Test
+  def setup
+    @solver = Day01.new('test/fixtures/day01_test.txt')
+  end
+
   def test_class_exists
     assert Object.const_defined?('Day01')
   end
 
   def test_proper_initialization
-    solver = Day01.new('test/fixtures/day01_test.txt')
+    assert @solver.respond_to?(:each_line)
+    assert @solver.respond_to?(:solve_part_1)
+    assert @solver.respond_to?(:solve_part_2)
+  end
 
-    assert solver.respond_to?(:each_line)
-    assert solver.respond_to?(:solve_part_1)
-    assert solver.respond_to?(:solve_part_2)
+  # Test parse_line indirectly
+  def test_parse_line
+    line_result = @solver.send(:parse_line, '3  4')
+    assert_equal line_result, [3, 4]
+
+    line_result = @solver.send(:parse_line, '4  3')
+    assert_equal line_result, [4, 3]
+
+    line_result = @solver.send(:parse_line, '44234  34234')
+    assert_equal line_result, [44_234, 34_234]
+  end
+
+  # Test lists indirectly
+  def test_lists
+    list1, list2 = @solver.send(:lists)
+    assert_equal list1, [1, 2, 3, 3, 3, 4]
+    assert_equal list2, [3, 3, 3, 4, 5, 9]
+  end
+
+  # Test list_differences indirectly
+  def test_list_differences
+    differences = @solver.send(:list_differences)
+    assert_equal differences, [2, 1, 0, 1, 2, 5]
+  end
+
+  def test_solve_part_1
+    assert_equal @solver.solve_part_1, 11
   end
 end
