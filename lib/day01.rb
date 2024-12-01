1e95f3896d4020ddd2ebac4b84b9a81d2cf9ae80
Alex Withington-Smith <alex@awithsmith.com>
2024-12-01 10:04:11 +0000

    working solution for day 2

--- lib/day01.rb
+++ lib/day01.rb
@@ -7,6 +7,16 @@ class Day01 < BaseSolver
     list_differences.sum
   end
 
+  def solve_part_2
+    list1, list2 = lists
+
+    occurrences2 = occurrences(list2)
+
+    list1.map do |n|
+      n * occurrences2[n]
+    end.sum
+  end
+
   private
 
   def lists
@@ -32,5 +42,11 @@ class Day01 < BaseSolver
     list1, list2 = lists
 
     list1.zip(list2).map { |a, b| (a - b).abs }
+  end
+
+  def occurrences(list)
+    list.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end
   end
 end
--- test/test_day01.rb
+++ test/test_day01.rb
@@ -17,7 +17,6 @@ class TestDay01 < Minitest::Test
     assert @solver.respond_to?(:solve_part_2)
   end
 
-  # Test parse_line indirectly
   def test_parse_line
     line_result = @solver.send(:parse_line, '3  4')
     assert_equal line_result, [3, 4]
@@ -29,14 +28,12 @@ class TestDay01 < Minitest::Test
     assert_equal line_result, [44_234, 34_234]
   end
 
-  # Test lists indirectly
   def test_lists
     list1, list2 = @solver.send(:lists)
     assert_equal list1, [1, 2, 3, 3, 3, 4]
     assert_equal list2, [3, 3, 3, 4, 5, 9]
   end
 
-  # Test list_differences indirectly
   def test_list_differences
     differences = @solver.send(:list_differences)
     assert_equal differences, [2, 1, 0, 1, 2, 5]
@@ -44,5 +41,17 @@ class TestDay01 < Minitest::Test
 
   def test_solve_part_1
     assert_equal @solver.solve_part_1, 11
+  end
+
+  def test_occurrences
+    occurrences = @solver.send(:occurrences, [1, 1, 1, 2, 2, 2, 2])
+
+    assert_equal occurrences[1], 3
+    assert_equal occurrences[2], 4
+    assert_equal occurrences[5], 0
+  end
+
+  def test_solve_part_2
+    assert_equal @solver.solve_part_2, 31
   end
 end
